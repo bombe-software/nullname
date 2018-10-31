ce6da8fbc560ec60d375cfd3204fd42aaaac752b
Vicroni <drasa_tec@hotmail.com>
2018-10-31 15:01:33 -0600

    Commit nada mas para ver que pasa

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
