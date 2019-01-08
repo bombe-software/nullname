603ba63bb640726ed2d03de21c12fb98a7cd9a8a
Vicroni <drasa_tec@hotmail.com>
2019-01-08 16:19:02 -0600

    Agregado fallido de navbar

--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -1,75 +1,61 @@
 import React, { Component } from 'react';
+import './../assets/landing_page.css';
 
 class LandingPage extends Component {
   render() {
     return (
       <div>
-        <nav class="navbar" role="navigation" aria-label="main navigation">
-          <div class="navbar-brand">
-            <a class="navbar-item" href="https://bulma.io">
-              <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-    </a>
+        <nav className="navbar is-transparent  is-fixed-top color-navbar" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            <a className="navbar-item" href="https://bulma.io">
+              <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+            </a>
 
-              <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-              </a>
-  </div>
+            <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
 
-            <div id="navbarBasicExample" class="navbar-menu">
-              <div class="navbar-start">
-                <a class="navbar-item">
-                  Home
-      </a>
-
-                <a class="navbar-item">
-                  Documentation
-      </a>
-
-                <div class="navbar-item has-dropdown is-hoverable">
-                  <a class="navbar-link">
-                    More
-        </a>
-
-                  <div class="navbar-dropdown">
-                    <a class="navbar-item">
-                      About
-          </a>
-                    <a class="navbar-item">
-                      Jobs
-          </a>
-                    <a class="navbar-item">
-                      Contact
-          </a>
-                    <hr class="navbar-divider">
-                      <a class="navbar-item">
-                        Report an issue
-          </a>
-        </div>
-                  </div>
-                </div>
-
-                <div class="navbar-end">
-                  <div class="navbar-item">
-                    <div class="buttons">
-                      <a class="button is-primary">
-                        <strong>Sign up</strong>
-                      </a>
-                      <a class="button is-light">
-                        Log in
-          </a>
-                    </div>
-                  </div>
+          <div id="navbarBasicExample" className="navbar-menu">
+            <div className="navbar-start">
+              <a className="navbar-item">Compara carreras</a>
+              <a className="navbar-item">Test vocacional </a>
+              <a className="navbar-item">Mapa </a>
+              <a className="navbar-item">Datos interesantes </a>
+              <div className="navbar-item has-dropdown is-hoverable">
+                <a className="navbar-link">Mas</a>
+                <div className="navbar-dropdown">
+                  <a className="navbar-item">Ayuda </a>
+                  <a className="navbar-item">Acerca de </a>
+                  <hr className="navbar-divider" />
+                  <a className="navbar-item">Reporta un problema</a>
                 </div>
               </div>
-</nav>
-
+            </div>
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <div className="control has-icons-right">
+                  <input className="input" type="email" placeholder="Buscar una carrera" />
+                  <span className="icon is-right">
+                    <i className="fas fa-search"></i>
+                  </span>
+                </div>
+              </div>
+            </div>
           </div>
-  
-          );
-        }
-    }
-     
-    export default LandingPage;
-  
+        </nav>
+        <section className="hero-image">
+          <div className="degradado"></div>
+          <div className="hero-text">
+            <h1 className="is-size-1">Busca informacion acerca de alguna carrera </h1>
+          </div>
+        </section>
+
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
