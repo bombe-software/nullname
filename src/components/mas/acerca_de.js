4f6a8e8015eac627d975c729ee0098ebbbec1072
MedinaVilla <diletalvezalaautopista@gmail.com>
2019-01-09 16:04:29 -0600

    Acerca de

--- src/components/mas/acerca_de.js
+++ src/components/mas/acerca_de.js
@@ -4,7 +4,18 @@ class AcercaDe extends Component {
     render() {
         return (
             <div>
-
+                <section class="hero is-primary">
+                    <div class="hero-body">
+                        <div class="container">
+                            <h1 class="title">
+                                Acerca de
+                    </h1>
+                        </div>
+                    </div>
+                </section>
+                <div class="box has-text-centered">Equipo de trabajo que desarrolló <strong>NullName</strong></div>
+                <img src="./.../assets/bombe.png" alt="Bombe Software" />
+                
             </div>
         );
     }
